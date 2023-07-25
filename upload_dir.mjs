35e1232ebb2391e1bfbfdc4ed65e69c711d54bf9
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-07-25 23:09:07 +0000

    test

--- upload_dir.mjs
+++ upload_dir.mjs
@@ -4,37 +4,36 @@ cd store-directory
 
 npm install nft.storage files-from-path
 */
-import { NFTStorage } from 'nft.storage'
-import { filesFromPath } from 'files-from-path'
+import {NFTStorage}from 'nft.storage'
+import {filesFromPaths} from 'files-from-path'
 import path from 'path'
 import fs from 'fs'
 
 //const token = ''
 
-async function main() {
-  // you'll probably want more sophisticated argument parsing in a real app
-  if (process.argv.length !== 4) {
+async function main(){
+  if(process.argv.length !== 4){
     console.error(`usage: ${process.argv[0]} ${process.argv[1]} <token> <directory-path>`)
   }
 
   const token = process.argv[2]
-  const storage = new NFTStorage({ token })
+  const storage = new NFTStorage({token})
 
   const directoryPath = process.argv[3]
-  const files = filesFromPath(directoryPath, {
+  const files = filesFromPaths(directoryPath, {
     pathPrefix: path.resolve(directoryPath), // see the note about pathPrefix below
     hidden: true, // use the default of false if you want to ignore files that start with '.'
-  })
+ })
 
   console.log(`storing file(s) from ${directoryPath}`)
   const cid = await storage.storeDirectory(files)
 
-  fs.appendFile('log.txt', directoryPath+': '+cid+'\n', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+  fs.appendFile('log.txt', directoryPath+'\t'+cid+'\n', function(err){
+    if(err) throw err
+    console.log('Saved!')
+ });
 
-  console.log({ cid })
+  console.log({cid})
   //const status = await storage.status(cid)
   //console.log(status)
 }
